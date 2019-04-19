a4085365d37630ca60b3b207b125d35cb24e4afd
Hohun Yoo <ehyoo97@gmail.com>
2019-04-19 19:06:07 -0400

    Adds all three neo4j queries

--- neo4j/queries.js
+++ neo4j/queries.js
@@ -24,11 +24,41 @@ const usersByBirthday = function findUsersWithBirthdaysInRange(lowerBound, upper
   // find orders between ... and ...
 
 const ordersByRange = function findOrdersWithTimestampInRange(customer, lowerBound, upperBound, callback) {
-  // const query = 'MATCH (n:User {email: $email})-[:'
+  let query = 'MATCH (n:User {email: $email})-[:PURCHASED] -> (o:Order) ';
+  query = query + 'WHERE $lowerBound < toInteger(o.timestamp) < $upperBound ';
+  query = query + 'RETURN n, o';
+  const queryParameters = {
+    email: customer,
+    lowerBound: lowerBound,
+    upperBound: upperBound,
+  };
+  const resultPromise = session.run(query, queryParameters);
+  resultPromise.then(res => {
+    console.log(res);
+    session.close();
+    callback();
+  }).catch(err => {
+    console.log(err);
+  });
 };
 
 // rec engine: for a user, query what other items they have given what they bought.
-const recEngine = function getRecommendedMerchandiseForUser(db, userId) {
+const recEngine = function getRecommendedMerchandiseForUser(customer, callback) {
+  let query = 'MATCH (u:User {email: $email}) -[:PURCHASED]-> (o:Order) -[:CONTAINS]-> (merch:Merchandise) '
+  query = query + '<-[:CONTAINS]- (relatedOrder:Order) -[:CONTAINS]-> (relatedMerch:Merchandise) '
+  query = query + 'RETURN DISTINCT relatedMerch'
+  const queryParameters = {
+    email: customer
+  };
+  const resultPromise = session.run(query, queryParameters);
+  resultPromise.then(res => {
+    session.close();
+    callback();
+  }).catch(err => {
+    console.log(err);
+  });
 }
 
-usersByBirthday(818125896, 818135996, () => driver.close());
+// usersByBirthday(818125896, 818135996, () => driver.close());
+// ordersByRange('jLHbGPug00', 1535060020, 1555060320, () => driver.close());
+// recEngine('jLHbGPug00', () => driver.close());
