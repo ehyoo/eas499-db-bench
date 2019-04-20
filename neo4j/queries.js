89dc388141a2e582d39de58e275ad53b0bf5053e
Hohun Yoo <ehyoo97@gmail.com>
2019-04-19 22:46:28 -0400

    Adds neo4j aggregateSpend and popularity queries

--- neo4j/queries.js
+++ neo4j/queries.js
@@ -5,6 +5,15 @@ const neo4j = require('neo4j-driver').v1;
 const driver = neo4j.driver('bolt://localhost:7687', neo4j.auth.basic('neo4j', 'asdfasdf')); // we're just on a localhost.
 const session = driver.session();
 
+const promiseHelper = function promiseHelper(resultPromise, cb) {
+  resultPromise.then(res => {
+    console.log(res);
+    cb();
+  }).catch(err => {
+    console.log(err);
+  });
+}
+
 const usersByBirthday = function findUsersWithBirthdaysInRange(lowerBound, upperBound, callback) {
   const query = 'MATCH (n:User) WHERE $lowerBound < toInteger(n.birthday) < $upperBound return n';
   const queryParameters = {
@@ -12,13 +21,7 @@ const usersByBirthday = function findUsersWithBirthdaysInRange(lowerBound, upper
     upperBound: upperBound,
   };
   const resultPromise = session.run(query, queryParameters);
-  resultPromise.then(res => {
-    console.log(res);
-    session.close();
-    callback();
-  }).catch(err => {
-    console.log(err);
-  });
+  promiseHelper(resultPromise, callback);
 };
   
   // find orders between ... and ...
@@ -33,13 +36,7 @@ const ordersByRange = function findOrdersWithTimestampInRange(customer, lowerBou
     upperBound: upperBound,
   };
   const resultPromise = session.run(query, queryParameters);
-  resultPromise.then(res => {
-    console.log(res);
-    session.close();
-    callback();
-  }).catch(err => {
-    console.log(err);
-  });
+  promiseHelper(resultPromise, callback);
 };
 
 // rec engine: for a user, query what other items they have given what they bought.
@@ -51,15 +48,41 @@ const recEngine = function getRecommendedMerchandiseForUser(customer, callback) 
     email: customer
   };
   const resultPromise = session.run(query, queryParameters);
+  promiseHelper(resultPromise, callback);
+}
+
+const aggregateSpend = function getAggregateSpendingAmountForUser(customer, callback) {
+  let query = 'MATCH (u:User {email: $email}) -[:PURCHASED]-> (o:Order) '
+  query = query + '-[:CONTAINS]-> (m:Merchandise) return sum(toInteger(m.price))'
+  const queryParameters = {
+    email: customer
+  };
+  const resultPromise = session.run(query, queryParameters);
   resultPromise.then(res => {
-    console.log(res);
-    console.log(res.records.length);
+    console.log(res.records[0]);
     callback();
   }).catch(err => {
     console.log(err);
   });
 }
 
+const popularity = function getTopThreeMostOrderedItemsForTimeRange(lowerBound, upperBound, callback) {
+  let query = 'MATCH (o:Order) -[c:CONTAINS]-> (m:Merchandise) '
+              + 'WHERE $lowerBound < toInteger(o.timestamp) < $upperBound '
+              + 'WITH m, count(c) AS numTimesOrdered '
+              + 'RETURN m, numTimesOrdered '
+              + 'ORDER BY numTimesOrdered DESC '
+              + 'LIMIT 3';
+  const queryParameters = {
+    lowerBound: lowerBound,
+    upperBound: upperBound
+  };
+  const resultPromise = session.run(query, queryParameters);
+  promiseHelper(resultPromise, callback);
+}
+
 // usersByBirthday(818125896, 818135996, () => driver.close());
 // ordersByRange('jLHbGPug00', 1535060020, 1555060320, () => driver.close());
-recEngine('jLHbGPug00', () => driver.close());
+// recEngine('jLHbGPug00', () => driver.close());
+// aggregateSpend('jLHbGPug00', () => driver.close());
+popularity(1511560866, 1522660866, () => driver.close());
